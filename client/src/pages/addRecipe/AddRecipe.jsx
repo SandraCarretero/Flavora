1fbf5b8e2b2d54bd8a83d23664ef5d0f01c6375f
SandraCarretero <sandracarretero24@gmail.com>
2024-08-30 16:35:53 +0200

    update

--- client/src/pages/addRecipe/AddRecipe.jsx
+++ client/src/pages/addRecipe/AddRecipe.jsx
@@ -5,14 +5,27 @@ import { URLS } from '../../constants/urls'; // Constantes de URL
 import { AuthContext } from '../../context/Auth.context'; // Importa el contexto
 import {
 	StyledButton,
-	StyledForm,
+	StyledFormGroup,
+	StyledFormTop,
+	StyledHr,
+	StyledImg,
+	StyledContent,
+	StyledIngredientsContainer,
 	StyledInput,
+	StyledLabel,
+	StyledPhotoBox,
+	StyledPhotoUpload,
+	StyledRecipeDetails,
+	StyledSection,
 	StyledSelect,
-	StyledTextarea
+	StyledStepsContainer,
+	StyledTextarea,
+	StyledTimeInputs
 } from './addRecipe.styles'; // Asegúrate de tener estos estilos
 
 const AddRecipe = () => {
 	const [name, setName] = useState('');
+	const [slice, setSlice] = useState('');
 	const [difficulty, setDifficulty] = useState('');
 	const [time, setTime] = useState('');
 	const [specialties, setSpecialties] = useState([]);
@@ -23,91 +36,188 @@ const AddRecipe = () => {
 	const { userLogged } = useContext(AuthContext);
 
 	return (
-		<StyledForm
-			onSubmit={e =>
-				handleSubmit(
-					e,
-					{
-						name,
-						difficulty,
-						time,
-						specialties,
-						course,
-						mealType,
-						steps
-					},
-					userLogged,
-					navigate
-				)
-			}
-		>
-			<StyledInput
-				type='text'
-				placeholder='Nombre de la receta'
-				value={name}
-				onChange={e => setName(e.target.value)}
-				required
-			/>
-			<StyledSelect
-				value={difficulty}
-				onChange={e => setDifficulty(e.target.value)}
-				required
-			>
-				<option value=''>Dificultad</option>
-				<option value='Fácil'>Fácil</option>
-				<option value='Media'>Media</option>
-				<option value='Difícil'>Difícil</option>
-			</StyledSelect>
-			<StyledInput
-				type='text'
-				placeholder='Tiempo de preparación'
-				value={time}
-				onChange={e => setTime(e.target.value)}
-				required
-			/>
-			<StyledSelect
-				value={specialties}
-				onChange={e =>
-					setSpecialties(
-						[...e.target.selectedOptions].map(option => option.value)
+		<StyledSection>
+			<h2>New recipe</h2>
+			<form
+				onSubmit={e =>
+					handleSubmit(
+						e,
+						{
+							name,
+							slice,
+							difficulty,
+							time,
+							specialties,
+							course,
+							mealType,
+							steps
+						},
+						userLogged,
+						navigate
 					)
 				}
 			>
-				<option value=''>Especiales</option>
-				<option value='Sin gluten'>Sin gluten</option>
-				<option value='Vegetariano'>Vegetariano</option>
-				<option value='Vegano'>Vegano</option>
-			</StyledSelect>
-			<StyledSelect
-				value={course}
-				onChange={e => setCourse(e.target.value)}
-				required
-			>
-				<option value=''>Tipo de plato</option>
-				<option value='Entrante'>Entrante</option>
-				<option value='Primer plato'>Primer plato</option>
-				<option value='Postre'>Postre</option>
-			</StyledSelect>
-			<StyledSelect
-				value={mealType}
-				onChange={e => setMealType(e.target.value)}
-				required
-			>
-				<option value=''>Tipo de comida</option>
-				<option value='Carne'>Carne</option>
-				<option value='Pescado'>Pescado</option>
-				<option value='Verdura'>Verdura</option>
-				<option value='Verdura'>Pasta</option>
-				<option value='Dulce'>Dulce</option>
-			</StyledSelect>
-			<StyledTextarea
-				placeholder='Pasos a seguir'
-				value={steps}
-				onChange={e => setSteps(e.target.value)}
-				required
-			/>
-			<StyledButton type='submit'>Añadir Receta</StyledButton>
-		</StyledForm>
+				<StyledFormTop>
+					<StyledPhotoUpload>
+						<StyledLabel htmlFor='photo'>Seleccionar foto</StyledLabel>
+						<input type='file' id='photo' accept='image/*' />
+						<StyledPhotoBox></StyledPhotoBox>
+					</StyledPhotoUpload>
+					<StyledRecipeDetails>
+						<StyledFormGroup>
+							<StyledLabel htmlFor='name'>Recipe name</StyledLabel>
+							<StyledInput
+								type='text'
+								placeholder='Recipe name'
+								value={name}
+								onChange={e => setName(e.target.value)}
+								required
+							/>
+						</StyledFormGroup>
+						<StyledFormGroup>
+							<StyledLabel htmlFor='slice'>Slices</StyledLabel>
+							<StyledInput
+								type='text'
+								placeholder='4'
+								value={slice}
+								onChange={e => setSlice(e.target.value)}
+								required
+							/>
+						</StyledFormGroup>
+						<StyledFormGroup>
+							<StyledLabel htmlFor='difficulty'>Difficulty</StyledLabel>
+							<StyledSelect
+								value={difficulty}
+								onChange={e => setDifficulty(e.target.value)}
+								required
+							>
+								<option value=''>Difficulty</option>
+								<option value='Fácil'>Easy</option>
+								<option value='Media'>Medium</option>
+								<option value='Difícil'>Difficult</option>
+							</StyledSelect>
+						</StyledFormGroup>
+
+						<StyledFormGroup>
+							<StyledLabel htmlFor='time'>Time</StyledLabel>
+							<StyledTimeInputs>
+								<StyledSelect
+									value={time}
+									onChange={e => setTime(e.target.value)}
+									required
+								>
+									<option value='0'>0h</option>
+									<option value='1'>1h</option>
+									<option value='2'>2h</option>
+									<option value='3'>3h</option>
+									<option value='4'>4h</option>
+									<option value='5'>5h</option>
+									<option value='6'>6h</option>
+								</StyledSelect>
+								<StyledSelect
+									value={time}
+									onChange={e => setTime(e.target.value)}
+									required
+								>
+									<option value='0'>0'</option>
+									<option value='5'>5'</option>
+									<option value='10'>10'</option>
+									<option value='15'>15'</option>
+									<option value='20'>20'</option>
+									<option value='25'>25'</option>
+									<option value='30'>30'</option>
+									<option value='35'>35'</option>
+									<option value='40'>40'</option>
+									<option value='45'>45'</option>
+									<option value='50'>50'</option>
+									<option value='55'>55'</option>
+								</StyledSelect>
+							</StyledTimeInputs>
+						</StyledFormGroup>
+						<StyledFormGroup>
+							<StyledLabel htmlFor='specials'>Specials</StyledLabel>
+							<StyledSelect
+								value={specialties}
+								onChange={e =>
+									setSpecialties(
+										[...e.target.selectedOptions].map(option => option.value)
+									)
+								}
+							>
+								<option value=''>Especiales</option>
+								<option value='Sin gluten'>Sin gluten</option>
+								<option value='Vegetariano'>Vegetariano</option>
+								<option value='Vegano'>Vegano</option>
+							</StyledSelect>
+						</StyledFormGroup>
+						<StyledFormGroup>
+							<StyledLabel htmlFor='type'>Type</StyledLabel>
+							<StyledSelect
+								value={course}
+								onChange={e => setCourse(e.target.value)}
+								required
+							>
+								<option value=''>Tipo de plato</option>
+								<option value='Entrante'>Entrante</option>
+								<option value='Primer plato'>Primer plato</option>
+								<option value='Postre'>Postre</option>
+							</StyledSelect>
+						</StyledFormGroup>
+						<StyledFormGroup>
+							<StyledLabel htmlFor='mealType'>Meal Type</StyledLabel>
+							<StyledSelect
+								value={mealType}
+								onChange={e => setMealType(e.target.value)}
+								required
+							>
+								<option value=''>Tipo de comida</option>
+								<option value='Carne'>Carne</option>
+								<option value='Pescado'>Pescado</option>
+								<option value='Verdura'>Verdura</option>
+								<option value='Pasta'>Pasta</option>
+								<option value='Verdura'>Arroz</option>
+								<option value='Postre'>Postre</option>
+								<option value='Batido'>Batido</option>
+							</StyledSelect>
+						</StyledFormGroup>
+					</StyledRecipeDetails>
+				</StyledFormTop>
+
+				<StyledHr />
+				<h3>Ingredients</h3>
+				<StyledIngredientsContainer>
+					<StyledContent>
+						<StyledInput type='number' placeholder='Amount'></StyledInput>
+						<StyledSelect>
+							<option value=''>Unit of measurement</option>
+							<option value='Gusto'>Al gusto</option>
+							<option value='Kg'>Kilogramos</option>
+							<option value='Cucharada'>Cucharada</option>
+							<option value='Cucharadita'>Cucharadita</option>
+							<option value='Pizca'>Pizca</option>
+						</StyledSelect>
+						<StyledInput type='text' placeholder='Ingredient'></StyledInput>
+						<StyledImg src='/images/delete.svg' alt='' />
+						<StyledImg src='/images/add.svg' alt='' />
+					</StyledContent>
+				</StyledIngredientsContainer>
+				<StyledHr />
+				<h3>Steps</h3>
+				<StyledStepsContainer>
+					<StyledContent>
+						<StyledTextarea
+							placeholder='Pasos a seguir'
+							value={steps}
+							onChange={e => setSteps(e.target.value)}
+							required
+						/>
+						<StyledImg src='/images/delete.svg' alt='' />
+						<StyledImg src='/images/add.svg' alt='' />
+					</StyledContent>
+				</StyledStepsContainer>
+				<StyledButton type='submit'>Añadir Receta</StyledButton>
+			</form>
+		</StyledSection>
 	);
 };
 
--- client/src/pages/addRecipe/addRecipe.styles.js
+++ client/src/pages/addRecipe/addRecipe.styles.js
@@ -1,23 +1,138 @@
 import styled from 'styled-components';
 
-const StyledForm = styled.form`
+const StyledSection = styled.section`
+	border-radius: 0.625rem;
+	box-shadow: 0 0 0.625rem rgba(0, 0, 0, 0.1);
+	padding: 1.25rem;
 	padding-top: 4.6875rem;
+	max-width: 80%;
+	margin: 0 auto;
+`;
+
+const StyledFormTop = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 1.25rem;
+`;
+
+const StyledPhotoUpload = styled.div`
+	flex: 1;
+	margin-right: 1.25rem;
+`;
+
+const StyledPhotoBox = styled.div`
+	width: 100%;
+	height: 15rem;
+	background-color: #f1f1f1;
+	border: 2px dashed #ccc;
+	border-radius: 0.625rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 0.875rem;
+	color: #666;
+	margin-top: 0.9375rem;
+`;
+
+const StyledRecipeDetails = styled.div`
+	flex: 2;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	height: calc(65px * 5.5);
+`;
+
+const StyledFormGroup = styled.div`
+	margin-bottom: 1.25rem;
+	width: 48%;
+`;
+
+const StyledLabel = styled.label`
+	display: block;
+	margin-bottom: 0.3125rem;
+	font-weight: bold;
 `;
 
 const StyledInput = styled.input`
-	/* Agrega tus estilos aquí */
+	width: calc(100% - 1.25rem);
+	padding: 0.625rem;
+	border: 0.0625rem solid #ccc;
+	border-radius: 0.25rem;
 `;
 
 const StyledSelect = styled.select`
-	/* Agrega tus estilos aquí */
+	width: calc(100% - 1.25rem);
+	padding: 0.625rem;
+	border: 0.0625rem solid #ccc;
+	border-radius: 0.25rem;
 `;
 
 const StyledTextarea = styled.textarea`
-	/* Agrega tus estilos aquí */
+	width: calc(100% - 1.25rem);
+	padding: 0.625rem;
+	border: 0.0625rem solid #ccc;
+	border-radius: 0.25rem;
+	resize: none;
+	field-sizing: content;
+`;
+
+const StyledTimeInputs = styled.div`
+	width: calc(100% - 1.25rem);
+	display: flex;
+	gap: 0.625rem;
+	justify-content: space-between;
+`;
+
+const StyledHr = styled.hr`
+	width: 80%;
+	border: none;
+	border-top: 1px solid #d6d6d683;
+`;
+
+const StyledIngredientsContainer = styled.div`
+	margin-bottom: 1.25rem;
+`;
+
+const StyledContent = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 0.625rem;
+	gap: 0.625rem;
+`;
+
+const StyledImg = styled.img`
+	width: 1.875rem;
+	cursor: pointer;
+`;
+const StyledStepsContainer = styled.div`
+	margin-bottom: 1.25rem;
 `;
 
 const StyledButton = styled.button`
-	/* Agrega tus estilos aquí */
+	background-color: #39db4a;
+	color: white;
+	border: none;
+	border-radius: 0.25rem;
+	padding: 0.625rem 1.25rem;
+	cursor: pointer;
 `;
 
-export { StyledForm, StyledInput, StyledSelect, StyledTextarea, StyledButton };
+export {
+	StyledSection,
+	StyledFormTop,
+	StyledPhotoUpload,
+	StyledPhotoBox,
+	StyledRecipeDetails,
+	StyledFormGroup,
+	StyledLabel,
+	StyledInput,
+	StyledSelect,
+	StyledTimeInputs,
+	StyledHr,
+	StyledIngredientsContainer,
+	StyledContent,
+	StyledImg,
+	StyledStepsContainer,
+	StyledTextarea,
+	StyledButton
+};
